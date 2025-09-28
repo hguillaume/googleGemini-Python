c29c17f2f7152c6e76f4a28487cfa46711a42ec7
Guillaume Hamilton <h_guillaume@hotmail.com>
2025-09-28 10:48:15 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# pip install google-genai
+
+# Create API Key Here: https://aistudio.google.com/app/apikey
+
+from google import genai
+from google.genai import types
+import googleGeminiAPIKey
+
+client = genai.Client(api_key=googleGeminiAPIKey.api_key)
+
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+)
+print(response.text)
