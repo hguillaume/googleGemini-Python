030700882e36b792e3063bef74aaf60d2cb8a1e6
Guillaume Hamilton <h_guillaume@hotmail.com>
2025-09-28 14:42:42 -0400

    Add link to list of Google Gemini Models

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import googleGeminiAPIKey
 
 client = genai.Client(api_key=googleGeminiAPIKey.api_key)
 
+# List of Google Gemini models:  https://ai.google.dev/gemini-api/docs/models#gemini-2.0-flash
 response = client.models.generate_content(
     model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+    # model='gemini-2.5-flash', contents='Why is the sky blue?'
+    # model='gemini-2.5-flash-lite', contents='Why is the sky blue?
+    # model='gemini-2.5-pro', contents='Why is the sky blue?'
 )
 print(response.text)
